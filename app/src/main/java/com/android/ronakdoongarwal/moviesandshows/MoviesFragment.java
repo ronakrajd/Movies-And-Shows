abd2569a5938adb99a8ce2a037a36107d752074c
ronakrajd <ro9460802458>
2016-06-13 21:24:45 +0530

    deleted  unused import statement

--- app/src/main/java/com/android/ronakdoongarwal/moviesandshows/MoviesFragment.java
+++ app/src/main/java/com/android/ronakdoongarwal/moviesandshows/MoviesFragment.java
@@ -3,7 +3,6 @@ package com.android.ronakdoongarwal.moviesandshows;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
 import android.util.DisplayMetrics;
